9a0c09acb646d3dc803a4f3c7989a77eac035a31
Zaid-maker <pzhafeez@gmail.com>
2024-10-17 22:54:31 +0500

    Add Link so we can redirect

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface AuthLayoutProps {
@@ -9,15 +10,17 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" alt="logo" width={152} height={56} />
-          <Button variant="secondary">
-            {pathname === "/sign-in" ? "Sign Up": "Sign In"}
+          <Button asChild variant="secondary">
+            <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+              {pathname === "/sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
           </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
