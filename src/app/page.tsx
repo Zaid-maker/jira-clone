94a909481e470b5061cfe662a13be0b0293cbdad
Zaid-maker <pzhafeez@gmail.com>
2024-10-19 12:38:44 +0500

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button variant="primary">Primary</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="teritary">Teritary</Button>
-      <Button disabled>Disabled</Button>
+      <Button variant="secondary">
+        <Link href="/sign-up">Sign Up</Link>
+      </Button>
+      <Button variant="primary">
+        <Link href="/sign-in">Sign In</Link>
+      </Button>
     </div>
   );
 }
