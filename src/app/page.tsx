6056fb17669abe6fcd97297aae360c491759e382
Zaid-maker <pzhafeez@gmail.com>
2024-10-22 19:23:27 +0500

    Logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout()
 
   useEffect(() => {
     if (!data && !isLoading) {
@@ -17,7 +19,10 @@ export default function Home() {
 
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Only logged in users can see this
+      Only logged in users can see this.
+      <Button onClick={() => mutate()}>
+        {isLoading ? "Loading..." : "Logout"}
+      </Button>
     </div>
   );
 }
---
+++ src/features/auth/api/use-logout.ts
@@ -0,0 +1,17 @@
+import { client } from "@/lib/rpc";
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
+
+export const useLogout = () => {
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout["$post"]();
+
+      return await response.json();
+    },
+  });
+
+  return mutation;
+};
