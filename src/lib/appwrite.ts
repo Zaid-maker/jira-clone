07e2b0453472bce554505fb30702d4af52d933a4
Zaid-maker <pzhafeez@gmail.com>
2024-10-23 00:24:50 +0500

    cleanup 🧹

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,14 +2,6 @@ import "server-only";
 
 import { Account, Client } from "node-appwrite";
 
-/**
- * Creates an instance of the Appwrite client that can be used to perform
- * administrative actions (i.e. actions that don't require authentication
- * as a user).
- *
- * @returns An object with an `account` property that is an instance of the
- *          Account service.
- */
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -17,13 +9,6 @@ export async function createAdminClient() {
     .setKey(process.env.NEXT_APPWRITE_KEY!);
 
   return {
-    /**
-     * An instance of the Account service.
-     *
-     * @remarks
-     * The Account service allows you to manage the current user session and
-     * account information.
-     */
     get account() {
       return new Account(client);
     },
