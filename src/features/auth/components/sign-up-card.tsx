300d8b27d7c476a9ae592a9b9494f5b73070e1b4
Zaid-maker <pzhafeez@gmail.com>
2024-10-25 00:24:17 +0500

    FIX

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,22 +1,19 @@
-"use client";
-
+import { z } from "zod";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import {
-  Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -28,7 +25,7 @@ import { registerSchema } from "../schemas";
 import { useRegister } from "../api/use-register";
 
 export const SignUpCard = () => {
-  const { mutate, isPending } = useRegister();
+  const { mutate } = useRegister();
 
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
@@ -44,25 +41,23 @@ export const SignUpCard = () => {
   };
 
   return (
-    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+    <div className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
         <CardTitle className="text-2xl">Sign Up</CardTitle>
         <CardDescription>
           By signing up, you agree to our{" "}
           <Link href="/privacy">
-            <span className="text-blue-700">Privacy policy</span>
+            <span className="text-blue-700">Privacy Policy</span>
           </Link>{" "}
-          and{" "}
+          and {""}
           <Link href="/terms">
             <span className="text-blue-700">Terms of Service</span>
           </Link>
         </CardDescription>
       </CardHeader>
-
       <div className="px-7">
         <DottedSeparator />
       </div>
-
       <CardContent className="p-7">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
@@ -91,7 +86,7 @@ export const SignUpCard = () => {
                     <Input
                       {...field}
                       type="email"
-                      placeholder="Enter email address"
+                      placeholder="Enter your email address"
                     />
                   </FormControl>
                   <FormMessage />
@@ -114,23 +109,21 @@ export const SignUpCard = () => {
                 </FormItem>
               )}
             />
-            <Button disabled={isPending} size="lg" className="w-full">
-              Sign Up
+            <Button disabled={false} size="lg" className="w-full">
+              Log In
             </Button>
           </form>
         </Form>
       </CardContent>
-
       <div className="px-7">
         <DottedSeparator />
       </div>
-
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
           variant="secondary"
           size="lg"
           className="w-full"
-          disabled={isPending}
+          disabled={false}
         >
           <FcGoogle className="mr-2 size-5" />
           Login with Google
@@ -139,25 +132,23 @@ export const SignUpCard = () => {
           variant="secondary"
           size="lg"
           className="w-full"
-          disabled={isPending}
+          disabled={false}
         >
           <FaGithub className="mr-2 size-5" />
-          Login with Github
+          Login with GitHub
         </Button>
       </CardContent>
-
       <div className="px-7">
         <DottedSeparator />
       </div>
-
       <CardContent className="p-7 flex items-center justify-center">
         <p>
-          Already have an account?{" "}
+          Already have an account?
           <Link href="/sign-in">
-            <span className="text-blue-700">Login</span>
+            <span className="text-blue-700">&nbsp;Sign In</span>
           </Link>
         </p>
       </CardContent>
-    </Card>
+    </div>
   );
 };
